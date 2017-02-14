2ca2e3528b6a277e50f51b5164ca61a7bf667052
Pavel Katovich <pkotovich@gmail.com>
2017-02-14 20:01:01 +0300

    webpack dev

--- client/app.js
+++ client/app.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello, world</h1>
+                <h1>Hello, webpack</h1>
             </div>
         );
     }
