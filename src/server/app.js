fdd03c5de98bccff93022066d0010801cc2ce065
Pavel Katovich <pkotovich@gmail.com>
2017-03-03 16:50:09 +0300

    using the same port for both server and client

--- src/server/app.js
+++ src/server/app.js
@@ -2,16 +2,31 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import * as db from './utils/db';
+import path from 'path';
 
 const app = express();
 
+const port = (process.env.PORT || 8080);
+
 db.connect();
+
+let publicDirPath = process.env.NODE_ENV == 'production' ? '/../public/client-build' :'/../../public/client-build';
+let indexPagePath = process.env.NODE_ENV == 'production' ? '/../public/index.html' :'/../../public/index.html';
+
+//serve static files from client-build directory
+app.use( '/public', express.static( path.join(__dirname, publicDirPath)) );
+
+//return index.html
+app.get('/', function(req, res) {
+    res.sendFile( path.join(__dirname, indexPagePath) );
+});
 
 app.use( bodyParser.json() );
 
 // Allow requests from any origin
 app.use(cors({ origin: '*' }));
 
+// -- REST 
 app.get('/tasks', (req, res)=> {
 	db.getTasks().then(data => res.send(data));
 });
@@ -27,7 +42,8 @@ app.post('/tasks', (req, res)=> {
 app.delete('/tasks/:id', (req, res) => {
     db.deleteTask(req.params.id).then(data => res.send(data));
 });
+// -- REST
 
-const server = app.listen(8080, ()=> {
-	console.log(`Server is up and running on port 8080`);
+const server = app.listen(port, ()=> {
+	console.log(`Server is up and running on port ${port} in ${process.env.NODE_ENV} mode`);
 });
