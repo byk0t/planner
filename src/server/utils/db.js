a1452e878c41c687d8e867b9e683b7330e56ed9b
Pavel Katovich <pkotovich@gmail.com>
2017-03-06 18:40:13 +0300

    heroku

--- src/server/utils/db.js
+++ src/server/utils/db.js
@@ -5,7 +5,7 @@ import '../models/Task';
 const Task = mongoose.model('Task');
 
 export function connect() {
-    mongoose.connect(`mongodb://localhost:27017/test`);
+    mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/test`);
 }
 
 export function getTasks() {
